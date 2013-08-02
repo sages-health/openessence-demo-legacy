c3b869153390d96f2efea9ad0dc94c337ed74d19
zarnamistry <zarna.mistry@jhuapl.edu>
2013-08-02 13:36:19 -0400

    Fixed bug: use findExact on store else it will find a wrong match by comparing partial string...

--- core/src/main/webapp/js/oe/app/widget/ComboColumn.js
+++ core/src/main/webapp/js/oe/app/widget/ComboColumn.js
@@ -23,46 +23,48 @@
  * INABILITY TO USE, THE MATERIAL, INCLUDING, BUT NOT LIMITED TO, ANY DAMAGES
  * FOR LOST PROFITS.
  */
-
+ 
 Ext.namespace("Ext.ux.grid", "Ext.ux.renderer");
 
 Ext.ux.grid.ComboColumn = Ext.extend(Ext.grid.Column, {
-
-    /**
+	 
+	/**
      * The grids id
      */
     gridId: undefined,
-
-    constructor: function (cfg) {
+ 
+    constructor: function(cfg){
         Ext.ux.grid.ComboColumn.superclass.constructor.call(this, cfg);
-
+ 
         // Check for editor using custom renderer otherwise default
-        this.renderer = (this.editor && this.editor.triggerAction) ? Ext.ux.renderer.ComboBoxRenderer(this.editor,
-            this.gridId) : function (value) {
-            return value; // TODO fix this formatting
-        };
+        this.renderer = (this.editor && this.editor.triggerAction) ? Ext.ux.renderer.ComboBoxRenderer(this.editor, this.gridId) : function(value) {return value;};
     }
 });
 Ext.grid.Column.types['combocolumn'] = Ext.ux.grid.ComboColumn;
-
+	 
 /**
  *  A renderer to show the display value when using combo boxes in editor grids (instead of value/fk)
  */
-Ext.ux.renderer.ComboBoxRenderer = function (combo, gridId) {
-    // Get the value from displayfield, else value
-    var getValue = function (value) {
-        var record = combo.store.getAt(combo.store.find(combo.valueField, value));
+Ext.ux.renderer.ComboBoxRenderer = function(combo, gridId) {
+	// Get the value from displayfield, else value
+    var getValue = function(value) {
+        var record = combo.store.getAt(combo.store.findExact(combo.valueField, value));
         if (record) {
             return record.get(combo.displayField);
         }
-
+        
         return value;
     };
-
-    return function (value) {
+	 
+    return function(value) {
         // Ensure combos store is loaded
-        if (combo.store.getCount() == 0 && gridId) {
-            combo.store.on('load', function () {
+    	 if (combo.store.getCount() == 0 && gridId && 
+    			 // Ensure we are adding onLoad only once per combo column
+    			 // This check was added because onLoad was added to the store 2^n times 
+    			 // where n is number of combo columns in this grid
+    			 (combo.onloadDefined==undefined || combo.onloadDefined === false)) {
+         	combo.onloadDefined = true;
+            combo.store.on('load', function() {
                     var grid = Ext.getCmp(gridId);
                     if (grid) {
                         grid.getView().refresh();
@@ -73,7 +75,7 @@ Ext.ux.renderer.ComboBoxRenderer = function (combo, gridId) {
             );
             return value;
         }
-
+ 
         return getValue(value);
     };
 };
